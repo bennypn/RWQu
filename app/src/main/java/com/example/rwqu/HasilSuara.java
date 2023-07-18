8754f2506fe7b2a539cdad4b3c5ebf6258ddf2d8
shalsaluthfita <shalsaluthfita10@gmail.com>
2023-07-18 10:43:54 +0700

    add all backround

---
+++ app/src/main/java/com/example/rwqu/HasilSuara.java
@@ -0,0 +1,14 @@
+package com.example.rwqu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilSuara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_suara3);
+    }
+}
