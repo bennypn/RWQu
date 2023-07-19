e6a08bd44a0697aa4c192469eff21210a132bbf3
shalsaluthfita <shalsaluthfita10@gmail.com>
2023-07-19 18:05:45 +0700

    button n input text

---
+++ app/src/main/java/com/example/rwqu/ScanKtp.java
@@ -0,0 +1,14 @@
+package com.example.rwqu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanKtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_ktp);
+    }
+}
